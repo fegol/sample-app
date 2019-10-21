29ceee111aa36f946f965f656ed8840217842489
fegol <fegol@mail.ru>
2019-10-21 15:49:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/miit/sampleapp/TextHandler.kt
@@ -0,0 +1,6 @@
+package ru.miit.sampleapp
+
+fun handleText(input: String): String {
+    // todo выполнять тут
+    return input
+}
---
+++ app/src/main/java/ru/miit/sampleapp/ui/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package ru.miit.sampleapp.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.main_fragment.*
+import ru.miit.sampleapp.R
+import ru.miit.sampleapp.handleText
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.main_fragment, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        execute_button.setOnClickListener {
+            result_text.text = handleText(enter_text.text.toString())
+        }
+    }
+
+}
